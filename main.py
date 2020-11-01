f0d5ff405998eefaaa5c1871bbd309860a418fc2
samueljsluo <samuel.js.luo@gmail.com>
2020-11-01 20:39:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sklearn.metrics import classification_report
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 print('GPU state:', device)
 PRETRAINED_MODEL_NAME = 'bert-base-uncased'
-BATCH_SIZE = 32
+BATCH_SIZE = 5
 EPOCH = 10
 
 bert = AutoModel.from_pretrained(PRETRAINED_MODEL_NAME)
@@ -26,18 +26,20 @@ class BERT(nn.Module):
         self.fc1 = nn.Linear(768, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 2)
+        self.fc4 = nn.Linear(768, 2)
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, sent_id, mask):
         _, cls_hs = self.bert(sent_id, attention_mask=mask)
-        x = self.fc1(cls_hs)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.dropout(x)
-        #output
-        x = self.fc3(x)
+        # x = self.fc1(cls_hs)
+        # x = self.relu(x)
+        # x = self.dropout(x)
+        # x = self.fc2(x)
+        # x = self.relu(x)
+        # x = self.dropout(x)
+        # #output
+        # x = self.fc3(x)
+        x = self.fc4(cls_hs)
         x = self.softmax(x)
         return x
 
@@ -81,7 +83,7 @@ def evaluate():
     total_preds = []
 
     for step, batch in enumerate(val_dataloader):
-        if step % 50 == 0 and not step == 0:
+        if step % 20 == 0 and not step == 0:
             print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(val_dataloader)))
         batch = [t.to(device) for t in batch]
         sent_id, mask, labels = batch
@@ -126,7 +128,7 @@ train_sampler = RandomSampler(train_data)
 train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=BATCH_SIZE)
 
 val_data = TensorDataset(val_seq, val_mask, val_y)
-val_sampler = SequentialSampler(val_data)
+val_sampler = RandomSampler(val_data)
 val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=BATCH_SIZE)
 
 # for param in bert.parameters():
